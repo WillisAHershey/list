9bba55235dba727454eec6c1b7661554330b414d
Willis A. Hershey <48898590+WillisAHershey@users.noreply.github.com>
2020-01-09 17:54:14 -0500

    Add files via upload

--- list.h
+++ list.h
@@ -19,7 +19,15 @@
 #ifndef LIST_FAILURE
 	#define LIST_FAILURE 0
 #endif
-#ifndef LISTTYPE_EQUAL
+#ifndef SIZEOF_LISTNODE_T
+	#define SIZEOF_LISTNODE_T sizeof(listNode_t)
+#endif
+#if !defined INCLUDE_QUEUE && !defined INCLUDE_STACK
+	#define INCLUDE_QUEUE
+	#define INCLUDE_STACK
+	#define INCLUDE_SEARCH_FUNCTIONS
+#endif
+#if defined INCLUDE_SEARCH_FUNCTIONS && !defined LISTTYPE_EQUAL
 	#define LISTTYPE_EQUAL(a,b) a==b
 #endif
 #ifdef __cplusplus
@@ -27,9 +35,11 @@ extern "C" {
 #endif
 
 typedef struct listNode{ //List node for both queue and stack
-  LISTTYPE data; //Nodes contain a data element of some arbitrary user-specified type
-  struct listNode *next; //and a pointer to another node
+  struct listNode *next; //Nodes contain a pointer to another node
+  LISTTYPE data; //And a data element of some arbitrary user-specified type
 }listNode_t;
+
+#ifdef INCLUDE_QUEUE
 
 typedef struct queue{ //Queue struct
   listNode_t *head; //Queues contain a pointer to the node at the beginning of the queue
@@ -38,13 +48,6 @@ typedef struct queue{ //Queue struct
   sem_t turn; //and in THREAD_SAFE mode, they have a semaphore to implement mutex
 #endif
 }queue_t;
-
-typedef struct stack{ //Stack struct
-  listNode_t *top; //Stacks contain a pointer to the node at the top of the stack
-#ifdef THREAD_SAFE
-  sem_t turn; //and in THREAD_SAFE mode, a semaphore to implement mutex
-#endif
-}stack_t;
 
 queue_t* newQueue(){ //Returns pointer to empty fifo queue. Returns NULL on error
   queue_t *out=(queue_t*)malloc(sizeof(queue_t)); 
@@ -64,7 +67,7 @@ queue_t* newQueue(){ //Returns pointer to empty fifo queue. Returns NULL on erro
 int queueAdd(queue_t *queue,LISTTYPE n){ //Adds valid LISTTYPE to valid queue. returns LIST_SUCCESS on success, and LIST_FAILURE on failure
   if(!queue||(INVALID_LISTTYPE(n))) //If NULL queue pointer or invalid LISTTYPE value
 	return LIST_FAILURE; //Return failure
-  listNode_t *hold=(listNode_t*)malloc(sizeof(listNode_t)); //Malloc space for a new node
+  listNode_t *hold=(listNode_t*)malloc(SIZEOF_LISTNODE_T); //Malloc space for a new node
   if(!hold) //If malloc failure
 	return LIST_FAILURE; //Return failure
   hold->next=NULL; //This will be the new end of the list so the next pointer must be null
@@ -81,14 +84,15 @@ int queueAdd(queue_t *queue,LISTTYPE n){ //Adds valid LISTTYPE to valid queue. r
 }
 
 int queueRemove(queue_t *queue,LISTTYPE *put){ //Places LISTTYPE from head of queue at address from input. Returns LIST_SUCCESS on success and LIST_FAIULRE on failure.
-  if(!queue||!put) //If NULL queue pointer or NULL LISTTYPE pointer
+  if(!queue) //If NULL queue pointer
 	return LIST_FAILURE; //Return failure
   wait(&queue->turn); //(This only does something in THREAD_SAFE mode. See macro definition of wait(c))
   if(!queue->head){ //If queue does not have a head, then it has no elements
 	post(&queue->turn); //(This only does something in THREAD_SAFE mode. See macro definition of post(c))
 	return LIST_FAILURE; //Return failure
   }
-  *put=queue->head->data; //Save the value at the head of the queue at the pointer address from input
+  if(put) //If address is not NULL
+	*put=queue->head->data; //Save the value at the head of the queue at the pointer address from input
   listNode_t *pt=queue->head; //Save a pointer to the current head (because we'll have to free the memory in a moment)
   queue->head=pt->next; //Set head to whatever the old head was pointing to (either another node or NULL)
   if(!queue->head) //If head has become null, it means this queue has no more elements
@@ -97,6 +101,23 @@ int queueRemove(queue_t *queue,LISTTYPE *put){ //Places LISTTYPE from head of qu
   free(pt); //Free the data from the old head
   return LIST_SUCCESS; //And return success
 }
+
+int freeQueue(queue_t *queue){ //Frees memory associated with the queue. Access to any data left in the queue will be lost. Returns LIST_SUCCESS/FAILURE
+  if(!queue) //If NULL queue pointer
+	return LIST_FAILURE; //Return failure
+  listNode_t *pt,*hold; //We access the data in the queue without using the mutex, so all other threads must finish before this function is called
+  for(pt=queue->head;pt;pt=hold){ //Progressively go through the queue and free the data from all the nodes. (Requires that queue is NULL terminated)
+	hold=pt->next;
+	free(pt);
+  }
+#ifdef THREAD_SAFE
+  sem_destroy(&queue->turn); //In THREAD_SAFE mode, make sure to destroy the semaphore, lest there be some memory leak in your system's implementation
+#endif
+  free(queue); //Free memory associated with the queue itself
+  return LIST_SUCCESS; //And return success
+}
+
+#ifdef INCLUDE_SEARCH_FUNCTIONS
 
 int queueRemoveValue(queue_t *queue,LISTTYPE value){ //Removes the first instance of a particular value from a valid stack. Returns LIST_SUCCESS/FAILURE
   if(!queue||(INVALID_LISTTYPE(value))) //If NULL queue pointer or invalid LISTTYPE value
@@ -140,20 +161,64 @@ int queueRemoveValue(queue_t *queue,LISTTYPE value){ //Removes the first instanc
   return LIST_FAILURE; //Return failure
 }
 
-int freeQueue(queue_t *queue){ //Frees memory associated with the queue. Access to any data left in the queue will be lost. Returns LIST_SUCCESS/FAILURE
-  if(!queue) //If NULL queue pointer
-	return LIST_FAILURE; //Return failure
-  listNode_t *pt,*hold; //We access the data in the queue without using the mutex, so all other threads must finish before this function is called
-  for(pt=queue->head;pt;pt=hold){ //Progressively go through the queue and free the data from all the nodes. (Requires that queue is NULL terminated)
-	hold=pt->next;
-	free(pt);
+int queueRemoveAll(queue_t *queue,LISTTYPE value){ //Removes all instances of some LISTTYPE value from a valid queue. Returns the number of values removed.
+  int out=0; //This will be our output value. It will be incremented every time we free a node.
+  if(!queue||(INVALID_LISTTYPE(value))) //If NULL queue pointer or invalid LISTTYPE value
+	return out; //Return zero
+  wait(&queue->turn); //(This only does something in THREAD_SAFE mode. See the macro definition of wait(c))
+  if(!queue->head){ //If the queue's head is NULL, then it has no elements
+	post(&queue->turn); //(This only does something in THREAD_SAFE mode. See the macro definition of post(c))
+	return out; //Return zero
   }
-#ifdef THREAD_SAFE
-  sem_destroy(&queue->turn); //In THREAD_SAFE mode, make sure to destroy the semaphore, lest there be some memory leak in your system's implementation
-#endif
-  free(queue); //Free memory associated with the queue itself
-  return LIST_SUCCESS; //And return success
+  listNode_t *pt,*hold; //pt will run through the list, and hold holds the next address for pt while pt frees a pointer
+  for(pt=queue->head;pt&&(LISTTYPE_EQUAL(pt->data,value));pt=hold){ //For every consecutive LISTTYPE that matches value beginning at the head
+	hold=pt->next;
+	free(pt); //Free the memory
+	++out; //And increment the output
+  }
+  if(!pt){ //If pt is now NULL (If every member of the list matched value)
+	queue->head=queue->tail=NULL; //Set head and tail both to NULL, as the list is now empty
+	post(&queue->turn); //(This only does something in THREAD_SAFE mode. See the macro definition of post(c))
+	return out; //And return the number of nodes removed
+  }
+  queue->head=pt; //Otherwise, the first value that doesn't match is the new head.
+  if(!pt->next){ //If the new head is also the tail, then we're done
+	post(&queue->turn); //(This only does something in THREAD_SAFE mode. See the macro deifnition of post(c))
+	return out; //Return the number of nodes removed
+  }
+  listNode_t *last=pt; //Last will represent the last node in the queue that was not removed.
+  for(pt=pt->next;pt!=queue->tail;pt=pt->next){ //For every other node in the queue that is not the tail
+	if(LISTTYPE_EQUAL(pt->data,value)){ //If it matches in value
+		hold=pt->next; //Save a pointer to the node after pt
+		free(pt); //Free the memory for the node that matches
+		++out; //Increment the output
+		last->next=hold; //Point the last node to the node after the node we removed
+		pt=last; //And point pt at the last node, so that it jumps to the correct place
+	}
+	else
+		last=pt; //Otherwise, this is the last node that wasn't removed
+  }
+  if(LISTTYPE_EQUAL(pt->data,value)){ //If the tail matches in value
+	free(pt); //Then we free the tail node
+	++out; //Increment the output
+	queue->tail=last; //Make the tail point to the last node that wasn't removed
+	last->next=NULL; //And NULL terminate the list
+  }
+  post(&queue->turn); //(This only does something in THREAD_SAFE mode. See the macro definition of post(c))
+  return out; //Return the number of nodes removed
 }
+
+#endif
+#endif
+
+#ifdef INCLUDE_STACK
+
+typedef struct stack{ //Stack struct
+  listNode_t *top; //Stacks contain a pointer to the node at the top of the stack
+#ifdef THREAD_SAFE
+  sem_t turn; //and in THREAD_SAFE mode, a semaphore to implement mutex
+#endif
+}stack_t;
 
 stack_t* newStack(){ //Returns pointer to new empty filo stack. Returns NULL on failure
   stack_t *out=(stack_t*)malloc(sizeof(stack_t)); //Malloc space for stack
@@ -172,7 +237,7 @@ stack_t* newStack(){ //Returns pointer to new empty filo stack. Returns NULL on 
 int stackPush(stack_t *stack,LISTTYPE n){ //Pushes valid LISTTYPE onto valid stack. Returns LIST_SUCCESS on success, and LIST_FAILURE on failure
   if(!stack||(INVALID_LISTTYPE(n))) //If NULL stack pointer, or invalid LISTTYPE value
 	return LIST_FAILURE; //Return failure
-  listNode_t *hold=(listNode_t*)malloc(sizeof(listNode_t)); //Malloc space for new node
+  listNode_t *hold=(listNode_t*)malloc(SIZEOF_LISTNODE_T); //Malloc space for new node
   if(!hold) //If malloc failure
 	return LIST_FAILURE; //Return failure
   hold->data=n; //Store the LISTTYPE value in the new node
@@ -184,20 +249,38 @@ int stackPush(stack_t *stack,LISTTYPE n){ //Pushes valid LISTTYPE onto valid sta
 }
 
 int stackPop(stack_t *stack,LISTTYPE *put){ //Pops LISTTYPE from top of the stack and saves it in address from input. Returns LISTTYPE_FAILURE on failure
-  if(!stack||!put) //If NULL stack pointer, or NULL LISTTYPE pointer
+  if(!stack) //If NULL stack pointer
 	return LIST_FAILURE; //Return failure
   wait(&stack->turn); //(This only does something in THREAD_SAFE mode. See the macro definition of wait(c))
   if(!stack->top){ //If the stack's top is NULL, then the stack is empty
 	post(&stack->turn); //(This only does something in THREAD_SAFE mode. See the macro definition of post(c))
 	return LIST_FAILURE; //Return failure
   }
-  *put=stack->top->data; //Store the data from the current top at the address from input
+  if(put) //If address is not NULL
+  	*put=stack->top->data; //Store the data from the current top at the address from input
   listNode_t *pt=stack->top; //Save a pointer to the current top (because we'll have to free the memory in a moment)
   stack->top=pt->next; //Make the stack's top point to whatever the old top was pointing to (either another node or NULL)
   post(&stack->turn); //(This only does something in THREAD_SAFE mode. See the macro definition of post(c))
   free(pt); //Free the data from the old stack's top
   return LIST_SUCCESS; //And return success
 }
+
+int freeStack(stack_t *stack){ //Frees memory associated with stack. Access to any data left in the stack will be lost. Returns LIST_SUCCESS/FAILURE
+  if(!stack) //If NULL stack pointer
+	return LIST_FAILURE; //Return failure
+  listNode_t *pt,*hold; //We access the stack's data without mutex, so all other threads must finish before this function is called
+  for(pt=stack->top;pt;pt=hold){ //Progressively go through the stack and free all of the nodes still in it
+	hold=pt->next;
+	free(pt);
+  }
+#ifdef THREAD_SAFE
+  sem_destroy(&stack->turn); //In THREAD_SAFE mode, make sure to destroy the semaphore, lest there be some memory leak in your system's implementation
+#endif
+  free(stack); //Free memory associated with the stack itself
+  return LIST_SUCCESS; //And return success
+}
+
+#ifdef INCLUDE_SEARCH_FUNCTIONS
 
 int stackRemoveValue(stack_t *stack,LISTTYPE value){ //Removes the first instance of a particular value from a valid stack. Returns LIST_SUCCESS/FAILURE
   if(!stack||(INVALID_LISTTYPE(value))) //If NULL queue pointer or invalid LISTTYPE value
@@ -228,20 +311,45 @@ int stackRemoveValue(stack_t *stack,LISTTYPE value){ //Removes the first instanc
   return LIST_FAILURE; //Return failure
 }
 
-int freeStack(stack_t *stack){ //Frees memory associated with stack. Access to any data left in the stack will be lost. Returns LIST_SUCCESS/FAILURE
-  if(!stack) //If NULL stack pointer
-	return LIST_FAILURE; //Return failure
-  listNode_t *pt,*hold; //We access the stack's data without mutex, so all other threads must finish before this function is called
-  for(pt=stack->top;pt;pt=hold){ //Progressively go through the stack and free all of the nodes still in it
-	hold=pt->next;
-	free(pt);
+int stackRemoveAll(stack_t *stack,LISTTYPE value){ //Removes all instances of some LISTTYPE value from valid stack. Returns number of values removed
+  int out=0; //This will serve as our output. It represents the number of values removed
+  if(!stack||(INVALID_LISTTYPE(value))) //If NULL stack pointer or invalid LISTTYPE value
+	return out; //Return zero
+  wait(&stack->turn); //(This only does something in THREAD_SAFE mode. See the macro definition of wait(c))
+  if(!stack->top){ //If the top of the stack is NULL, then it has no values
+	post(&stack->turn); //(This only does something in THREAD_SAFE mode. See the macro definition of post(c))
+	return out; //Return zero
   }
-#ifdef THREAD_SAFE
-  sem_destroy(&stack->turn); //In THREAD_SAFE mode, make sure to destroy the semaphore, lest there be some memory leak in your system's implementation
-#endif
-  free(stack); //Free memory associated with the stack itself
-  return LIST_SUCCESS; //And return success
+  listNode_t *pt,*hold; //Pt will iterate through the nodes in the stack, hold will hold the spot for point to jump to
+  for(pt=stack->top;pt&&(LISTTYPE_EQUAL(pt->data,value));pt=hold){ //For each consecutive matching value beginning at the top of stack
+	hold=pt->next;
+	free(pt); //Free the node
+	++out; //And increment the output
+  }
+  if(!pt){ //If every value in the stack matched
+	stack->top=NULL; //Set the top to NULL to indicate the stack is empty
+	post(&stack->turn); //(This only does something in THREAD_SAFE mode. See the macro defninition of post(c))
+	return out; //And return the number of values removed
+  }
+  stack->top=pt; //Otherwise, this first node that didn't have a matching value is the new top
+  listNode_t *last=pt; //Last will represent the last value not removed, so we set it to the new top
+  for(pt=pt->next;pt;pt=pt->next){ //For every remaining valaue in the stack
+	if(LISTTYPE_EQUAL(pt->data,value)){ //If it matches in value
+		hold=pt->next;
+		free(pt); //Free it
+		++out; //Increment the output
+		last->next=hold; //Point the last node that didn't match to the one after the one that did
+		pt=last;
+	}
+	else
+		last=pt; //Otherwise, this is the last node that wasn't removed
+  }
+  post(&stack->turn); //(This only does something in THREAD_SAFE mode. See the macro definition of post(c))
+  return out; //Return the number of values removed
 }
+
+#endif
+#endif
 
 #undef wait
 #undef post
